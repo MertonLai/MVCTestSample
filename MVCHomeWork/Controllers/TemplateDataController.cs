6be6ddfdd28fc374bd33d14330004ce097143e02
Merton Lai <leader4merton@gmail.com>
2017-11-09 12:14:23 +0800

    更新Template 取值方式

---
+++ MVCHomeWork/Controllers/TemplateDataController.cs
@@ -0,0 +1,56 @@
+﻿using MVCHomeWork.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHomeWork.Controllers
+{
+    public class TemplateDataController : Controller {
+
+        IBLL _BLL = new SysUtility();
+
+        public IEnumerable<SelectListItem> CustCardList(int? SelectValue) {
+            int _TmpData = 0;
+            if (SelectValue.HasValue) {
+                _TmpData = SelectValue.Value;
+            }
+            return _BLL.GetCustTypesList(_TmpData);
+
+
+        }
+
+        public string GetCustCartTypeName(int? SelectValue) {
+            int _TmpData = 0;
+            if (SelectValue.HasValue) {
+                _TmpData = SelectValue.Value;
+            }
+
+            return _BLL.GetCustCartTypeName(_TmpData);
+        }
+
+        public string GetJobTitleName(string SelectText) {
+            string _TmpData = "";
+            if (!string.IsNullOrWhiteSpace(SelectText)) {
+                _TmpData = SelectText;
+            }
+            return _BLL.GetJobTitleName(_TmpData);
+        }
+
+        public IEnumerable<SelectListItem> GetJobTitleList(string SelectText) {
+            string _TmpData = "";
+            if (!string.IsNullOrWhiteSpace(SelectText)) {
+                _TmpData = SelectText;
+            }
+            return _BLL.GetJobTitleList(_TmpData);
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                _BLL = null;
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
