d32a81b44c192424adc8720e18426acdf2258523
Merton Lai <leader4merton@gmail.com>
2017-11-14 22:50:36 +0800

    1.修改Custom查詢條件，移入MODEL 2.取消Custom Grid的partial View

---
+++ MVCHomeWork/Controllers/Base/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using MVCHomeWork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHomeWork.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected CustomEntities db = new CustomEntities();
+        protected int PageCount = 20;
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
